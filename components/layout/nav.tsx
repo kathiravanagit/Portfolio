ac0edb6fa936ce4753d9a0f77436df36973a62fc
kathiravanagit <kathiravanawork@gmail.com>
2026-09-20 17:31:21 +0530

    Restore Home link in navbar

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -20,6 +20,7 @@ type NavItem = {
 };
 
 const NAV_ITEMS: readonly NavItem[] = [
+  { label: "Home", href: "/" },
   { label: "Projects", href: "/projects" },
   { label: "About", href: "/about" },
 ];
